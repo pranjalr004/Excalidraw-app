1fc8945c21a48846abe9a9e4cc664fcaf8514cb8
pranjalr004 <pranjalr376@gmail.com>
2025-02-01 19:45:35 +0530

    Added DB imports

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 import { middleware } from "./middleware"
 import {JWT_SECRET} from "@repo/backend-common/config"
 import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
-
+import { prismaClient } from "@repo/db/client"
 const app=express()
 
 app.post("/signup",(req,res)=>{
