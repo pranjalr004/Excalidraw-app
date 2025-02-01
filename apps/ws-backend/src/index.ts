c1f4b8a426c008de4e0ddfefbb3c0a3baa8a3011
pranjalr004 <pranjalr376@gmail.com>
2025-02-01 08:01:40 +0530

    Added DB

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import {WebSocketServer} from  "ws"
 import jwt, { JwtPayload } from 'jsonwebtoken'
-import { JWT_SECRET } from "./config"
+import {JWT_SECRET} from "@repo/backend-common/config"
 
 const wss=new WebSocketServer({port:8080})
 
@@ -12,6 +12,11 @@ wss.on('connection',function connection(ws,request){
     const queryParams=new URLSearchParams(url.split('?')[1])
     const token=queryParams.get('token') || "";
     const decoded=jwt.verify(token,JWT_SECRET)
+
+    if(typeof decoded == "string"){
+        ws.close()
+        return
+    }
 
 
     if(!decoded || !(decoded as JwtPayload).userId){
